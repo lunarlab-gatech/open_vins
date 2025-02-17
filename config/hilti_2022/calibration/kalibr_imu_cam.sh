36b555132c88cd1f8c09439613d03889c761397b
Lu Gan <lgan@gatech.edu>
2025-02-17 00:12:49 -0500

    re-calibrated hilti 2022 imu-cam, 4-cam vio running

---
+++ config/hilti_2022/calibration/kalibr_imu_cam.sh
@@ -0,0 +1,6 @@
+rosrun kalibr kalibr_calibrate_imu_camera \
+        --target april_grid_2500x1500_7x12_15cm.yaml \
+        --imu imu_alphasense.yaml \
+        --imu-models calibrated \
+        --cam calib_03_2022-03-02-11-27-22-camchain.yaml \
+        --bag calib_03_2022-03-02-11-27-22.bag
