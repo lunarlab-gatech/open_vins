c0523bd1bcaa28aa3b03fb281c44dc12635b923f
Daniel Butterfield <danielcbutter@gmail.com>
2026-01-07 10:51:30 -0500

    Dockerfiles to mimic other LunarLab setups

---
+++ docker/build_container.sh
@@ -0,0 +1,1 @@
+docker build -t open_vins .
---
+++ docker/run_container.sh
@@ -0,0 +1,25 @@
+DATA_DIR='/media/dbutterfield3/T73'
+WS_DIR='/home/dbutterfield3/Research/ros_workspaces/open_vins_ws/'
+
+docker run -it \
+    --name="open_vins" \
+    --net="host" \
+    --gpus="all" \
+    --privileged \
+    --device /dev/dri \
+    --workdir="/home/$USER/open_vins_ws" \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="XAUTHORITY=/tmp/.Xauthority" \
+    --env="XDG_RUNTIME_DIR=/tmp/runtime-$USER" \
+    --env="USER_ID=$(id -u)" \
+    --env="GROUP_ID=$(id -g)" \
+    --volume="$DATA_DIR:/home/$USER/data:rw" \
+    --volume="$WS_DIR:/home/$USER/open_vins_ws:rw" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="/tmp/runtime-$USER:/tmp/runtime-$USER" \
+    --volume="$XAUTHORITY:/tmp/.Xauthority:ro" \
+    --volume="$HOME/.bash_aliases:/root/.bash_aliases" \
+    --volume="$HOME/.ssh:/root/.ssh:ro" \
+    open_vins \
+    /bin/bash
